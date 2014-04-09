fea62dc90a5ad080cc08291759e7104baf00caae
poofytoo <victorhung92@gmail.com>
2014-04-09 13:00:03 -0400

    4 functions fully implemented. exciting.

--- wb/lib/commands.js
+++ wb/lib/commands.js
@@ -1,39 +1,49 @@
 function commands(service) {
-  this.s = service;
   this.characters = false;
-
+  
+  var animateQueue = [];
+  var s = service;
+  
+  function animate() {
+    // Send the Flicker Animation
+    if (animateQueue.indexOf('flicker') != -1) {
+      if (Math.random() > 0.5) { 
+        packet = [0xAA, 0xFF, 255, 255, 255];
+      } else {
+        packet = [0xAA, 0xFF, 0, 0, 0];
+      }
+      s.serialwrite(packet);
+    }
+  }
+  
+  this.t = setInterval(animate, 83); // roughly 12fps
+  
   this.allOff = function() {
     packet = [0xAA, 0xFF, 0, 0, 0];
-    this.s.serialwrite(packetToReturn(packet));
+    s.serialwrite(packetToReturn(packet));
   }
 
   this.allOn = function(r, g, b) {
     packet = [0xAA, 0xFF, r, g, b];
-    //this.s.serialwrite(packetToReturn(packet));
+    //s.serialwrite(packetToReturn(packet));
   }
 
   this.oneOff = function(light) {
     packet = [0xAA, light, 0, 0, 0];
-    this.s.serialwrite(packetToReturn(packet));
+    s.serialwrite(packetToReturn(packet));
   }
 
   this.oneOn = function(light, r, g, b) {
     packet = [0xAA, light, r, g, b];
-    this.s.serialwrite(packet);
+    s.serialwrite(packet);
   }
 
-  this.oneOn = function(light, r, g, b) {
-    packet = [0xAA, light, r, g, b];
-    this.s.serialwrite(packet);
+  this.flicker = function() {
+    animateQueue.push('flicker');
   }
   
-  this.flicker = function() {
-    console.log('flicker!');
-    for (i = 0; i < 10; i ++) {
-      console.log(i);
-      //packet = [0xAA, 2, i, i, i];
-      //this.s.serialwrite(packet);
-    }
+  this.kill = function() {
+    animateQueue = [];
   }
   
   packetToReturn = function(packet) {
@@ -53,6 +63,7 @@ function commands(service) {
     return bytes;
   }
 
+  // no longer used
   packetToCharArray = function(packet) {
     characters = "";
     for (k in packet) {
